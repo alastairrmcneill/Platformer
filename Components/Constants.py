d8099cc36e22c4d1b8814010400d7cff71aa5b88
Ali <alastair.r.mcneill@gmail.com>
2021-01-25 12:53:30 +0000

    First commit

---
+++ Components/Constants.py
@@ -0,0 +1,22 @@
+"""
+Constants for the game
+"""
+import os
+import pygame
+pygame.font.init()
+
+# Window variables
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+
+# Colours
+
+# Paths
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+IMGS_PATH = os.path.join(BASE_PATH, "Imgs")
+FONTS_PATH = os.path.join(BASE_PATH, "Fonts")
+
+# Images
+
+# Fonts
+
